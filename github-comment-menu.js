96e4a2ca7739bdc835b5d5c845ce79c3d775c5bb
lauripiisang <noskiller@gmail.com>
2014-09-10 23:55:02 +0300

    Fixes Issue #2, Cleaning code
    Fixed the bug, made code more readable, added comments.
    Performance: The script no longer wastes time if no comments.

--- github-comment-menu.js
+++ github-comment-menu.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Github Comment Menu
 // @namespace http://iknowur.name/
-// @version 0.1
+// @version 0.1.2
 // @description Adds a quick-link menu to comments in commit view
 // @match https://github.com/*/commit/*
 // @copyright 2014+, Lauri Piisang
@@ -10,8 +10,16 @@
 // @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 $(document).ready(function () {
-    var commentMenuHtml;
-    commentMenuHtml = '<li id="comment-menu-list" class="subscription">\
+    var comments = $('.comment.timeline-comment:visible');
+    var commentCount = comments.length;
+    
+    //If there are no comments, do not waste time
+    if(!commentCount)
+        return;
+        
+    //Let's create the menu
+    
+    var commentMenuHtml = '<li id="comment-menu-list" class="subscription">\
 <div class="select-menu js-menu-container js-select-menu">\
 <a class="social-count js-social-count" id="comment-count" href="#">\
 X\
@@ -39,15 +47,15 @@ Comments\
         <span class="description">EXCERPTHOLDER</span>\
     </div>\
 </a>';
-    var comments = $('.comment.timeline-comment:visible');
-    var commentCount = comments.length;
-    //Let's create the menu
+    
     $('.pagehead-actions').prepend(commentMenuHtml);
     $('#comment-count').text(commentCount);
     var submenu = $('#comment-menu-list .select-menu-list');
     var realItemHtml;
     var fileline;
     var comment;
+    var excerpt;
+    
     for (var i = 0; i < commentCount; i++) {
         comment = comments[i];
         //First up, replace placeholders with actual values
@@ -59,18 +67,24 @@ Comments\
         excerpt = $(comment).find('.comment-body').text();
         excerpt = excerpt.substr(0, excerpt.lastIndexOf(' ', 75)) + '...';
         realItemHtml = realItemHtml.replace("EXCERPTHOLDER", excerpt.toString());
-        fileline = $(comment).parents('tr').prev().data('path');
-        if (!fileline || fileline == "undefined")
+        
+        //This selects the previous line of code. Lines of code have a 'b' element that contains related information as data
+        var commentData = $($(comment).parents('tr.inline-comments')[0]).prev('tr').find('b').data();
+        
+        if (typeof commentData == "undefined" || !commentData)
             fileline = "commit general";
         else {
-            var filePathPart = fileline.split('/');
+            var filePath = commentData.path;
+            var filePathPart = filePath.split('/');
             var fileName = filePathPart[filePathPart.length - 1];
-            fileline = fileName + ':' + $(comment).parents('tr').prev().data('line');
+            fileline = fileName + ':' + commentData.line;
         }
 
         realItemHtml = realItemHtml.replace("FILELINEHOLDER", fileline);
 
+        //"real item" is constructed, add it to our menu.
         submenu.append(realItemHtml);
+        //reset to start fresh.
         realItemHtml = "";
     }
 });
